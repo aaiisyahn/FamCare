6f179e19543ba74eb6fd249dca6f95113e7208c1
aaiisyahn <you@example.com>
2024-03-28 08:32:04 +0700

    Commit package and activity

---
+++ app/src/main/java/com/app/famcare/view/bookmark/BookmarkActivity.kt
@@ -0,0 +1,12 @@
+package com.app.famcare.view.bookmark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.famcare.R
+
+class BookmarkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookmark)
+    }
+}
---
+++ app/src/main/java/com/app/famcare/view/detailpost/DetailPostActivity.kt
@@ -0,0 +1,12 @@
+package com.app.famcare.view.detailpost
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.famcare.R
+
+class DetailPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_post)
+    }
+}
