e0dc98647da6ee5307acb89f01ea9e2d0c379690
aaiisyahn <you@example.com>
2024-03-29 13:42:35 +0700

    Commit changes navbar

--- app/src/main/java/com/app/famcare/view/main/MainActivity.kt
+++ app/src/main/java/com/app/famcare/view/main/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.app.famcare.view.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.app.famcare.R
+import com.app.famcare.view.bookmark.BookmarkActivity
+import com.app.famcare.view.maps.MapsActivity
+import com.app.famcare.view.profile.ProfileActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Kode Navigasi untuk Bottom Bar
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.page_1 -> {
+                    true
+                }
+                R.id.page_2 -> {
+                    val intent = Intent(this, BookmarkActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.page_3 -> {
+                    val intent = Intent(this, MapsActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.page_4 -> {
+                    val intent = Intent(this, ProfileActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
